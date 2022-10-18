a54f32c1b115cc8e924ebf02cc0c8e8d0caa2bc7
Caio Henrique Alves <50040463+CaioHAlves@users.noreply.github.com>
2022-10-18 20:57:25 -0300

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import { HeaderComponent, OptionsDiv, StyledButton, MyName } from './styles'
 export function Header() {
 
   const returnToSite = () => {
-    window.open("caiohalves.pages.dev")
+    window.open("https://caiohalves.pages.dev")
   }
 
   return (
